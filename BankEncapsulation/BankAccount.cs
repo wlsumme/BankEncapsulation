872ec89de1822d1608eb1f2a1cbad5777f80b46f
William Summe <wlsumme@gmail.com>
2024-05-20 09:53:30 -0500

    done

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            account.Deposit(123);
+            
+
+            var balance = account.GetBalance();
+            Console.WriteLine(balance);
         }
     }
 }
