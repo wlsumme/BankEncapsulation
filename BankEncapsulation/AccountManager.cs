baa7d98d2f94280e60ddc2ac8174b7e2262a0deb
William Summe <wlsumme@gmail.com>
2024-05-23 10:03:48 -0500

    I Think i got it

---
+++ BankEncapsulation/AccountManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class AccountManager
+    {
+
+
+        public void ATM(BankAccount account)
+        {
+            
+            {
+                var williamsAccount = new BankAccount();
+                var macksAccount = new BankAccount();
+
+                var manager = new AccountManager();
+
+                manager.ATM(williamsAccount);
+            }
+        }  
+    }
+}
--- BankEncapsulation/obj/Debug/net6.0/BankEncapsulation.AssemblyInfo.cs
+++ BankEncapsulation/obj/Debug/net6.0/BankEncapsulation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankEncapsulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872ec89de1822d1608eb1f2a1cbad5777f80b46f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c04e5c39fc120d6b10d63e0b4a658b98ef7fe1cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankEncapsulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankEncapsulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
